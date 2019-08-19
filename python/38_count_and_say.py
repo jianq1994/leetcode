d68b5ac6359a06e144fc6273978d91c5f73093f1
Jian Qian <jianq1994@gmail.com>
2019-08-19 22:46:19 +0200

    git commit 38_count_and_say.py -m ">80%"

--- python/38_count_and_say.py
+++ python/38_count_and_say.py
@@ -1,3 +1,32 @@
+# 44-92,6
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return '1'
+        cac, ncac = '1', ''
+        for i in range(n-1):
+            count = 1
+            # count = 0
+            for i in range(len(cac)-1):
+                if cac[i] == cac[i+1]:
+                    count += 1
+                else:
+                    ncac += str(count)+cac[i]
+                    count = 1
+            ncac += str(count) + cac[-1]
+            cac = ncac
+            ncac = ""
+        return cac
+
+
+
+# more logical
+
 class Solution:
     def countAndSay(self, n):
         """
